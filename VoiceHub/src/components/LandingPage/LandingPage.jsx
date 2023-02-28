2a71e9616d0afd7e4912b9535cb3f0b59a8f542a
Loloming <damianbruzzoni@gmail.com>
2023-02-28 00:47:55 -0300

    fix: h3 is in the middle page

--- VoiceHub/src/components/LandingPage/LandingPage.jsx
+++ VoiceHub/src/components/LandingPage/LandingPage.jsx
@@ -5,11 +5,11 @@ export default function LandinPage() {
   return (
     <>
       <div className="shadow-2xl" />
-      <article className="bg-gradient-to-tr  from-sixty-percent via-thirty-percent to-sixty-percent min-h-screen">
-        <h3 className="text-4xl font-semibold text-teal-50">
+      <article className="flex flex-col justify-center pl-3 bg-gradient-to-tr  from-sixty-percent via-thirty-percent to-sixty-percent min-h-screen">
+        <h3 className="text-5xl font-semibold text-teal-50">
           An amazing app based on audio and streaming.
         </h3>
-        <h4 className="text-8xl font-semibold text-ten-percent"> click me </h4>
+        <h4 className="text-5xl pl-5 font-semibold text-ten-percent"> click me </h4>
       </article>
     </>
   );
