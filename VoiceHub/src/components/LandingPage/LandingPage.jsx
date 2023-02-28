769c16c4653a77222f1888d95493118085328d64
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-02-27 22:57:13 -0500

    null

--- VoiceHub/src/components/LandingPage/LandingPage.jsx
+++ VoiceHub/src/components/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ export default function LandinPage() {
   return (
     <>
       <div className="shadow-2xl" />
-      <article className="bg-gradient-to-tr  from-sixty-percent via-thirty-percent to-sixty-percent min-h-screen">
+      <article className="bg-sixty-percent min-h-screen">
         <h3 className="text-4xl font-semibold text-teal-50">
           An amazing app based on audio and streaming.
         </h3>
