f89aa540cb1f4cc7ad34a1bd7dbb1be36a9ec31d
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-03-03 00:29:47 -0500

    no changes

--- VoiceHub/src/components/Register/Register.jsx
+++ VoiceHub/src/components/Register/Register.jsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 
 export default function Register() {
+  const [user, setUser] = useState({
+    username: "",
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        username: '',
-        name: '',
-        lastname: '',
-        email: '',
-        password: ''
-    })
-
-    return (
-       <h1>Register component</h1>
-    )
+  return <h1>Register component</h1>;
 }
