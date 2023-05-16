2c044bd5dcf97ff71ce8e74994d6d9d33c51f77e
RinnAnd <107440163+RinnAnd@users.noreply.github.com>
2023-05-16 09:37:29 -0500

    none

--- VoiceHub/src/components/Home/Home.jsx
+++ VoiceHub/src/components/Home/Home.jsx
@@ -8,16 +8,16 @@ import Upload from "../Upload/Upload";
 
 export default function Home() {
   const [modal, setModal] = useState(false);
-  const [published, setPublished] = useState(false)
+  const [published, setPublished] = useState(false);
 
   useEffect(() => {
-    if(published) {
-        setTimeout(() => {
-            setModal(false)
-            setPublished(false)
-        }, 2000);
+    if (published) {
+      setTimeout(() => {
+        setModal(false);
+        setPublished(false);
+      }, 2000);
     }
-  }, [published])
+  }, [published]);
 
   const dispatch = useDispatch();
 
@@ -28,12 +28,23 @@ export default function Home() {
   return (
     <div className="flex flex-col h-full justify-start bg-gradient-to-b from-sixty-percent-home to-sixty-percent min-h-screen">
       <Header />
-      {modal ? <div className="fixed inset-0 transition-opacity z-40 flex justify-center items-center">
-        <div className="absolute inset-0 bg-gray-800 opacity-75 z-40" onClick={() => setModal(false)}></div>
-        <Upload setPublished={setPublished} published={published} />
-      </div> : <></>}
+      {modal ? (
+        <div className="fixed inset-0 transition-opacity z-40 flex justify-center items-center">
+          <div
+            className="absolute inset-0 bg-gray-800 opacity-75 z-40"
+            onClick={() => setModal(false)}
+          ></div>
+          <Upload setPublished={setPublished} published={published} />
+        </div>
+      ) : (
+        <></>
+      )}
       <div className="p-2">
-        <button className="bg-ten-percent text-white p-2 rounded-md" w="3rem" onClick={() => setModal(!modal)}>
+        <button
+          className="bg-ten-percent text-white p-2 rounded-md"
+          w="3rem"
+          onClick={() => setModal(!modal)}
+        >
           Create
         </button>
       </div>
